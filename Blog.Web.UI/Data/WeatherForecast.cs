a48b5c027984bbeb1b1cc1e1e096b7b1b4d567e2
Brett Goulder <goulderba@gmail.com>
2023-10-09 20:13:08 -0400

    chore: remove unused

---
+++ Blog.Web.UI/Data/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Blog.Web.UI.Data;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF { get; set; }
+
+    public string? Summary { get; set; }
+}
